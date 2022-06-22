a020ee2728cb72480013faf435be4f4d55dee636
lucas-pedrosa <lucas.dias.pedrosa@gmail.com>
2022-06-22 16:42:58 -0300

    Correções de css

--- js/firebase.js
+++ js/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase
 import { getFirestore, doc, getDoc, setDoc, collection, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASv5h92BfWQ1LfYVeKLVjeCw0pbzCtizQ",
-  authDomain: "webdashboard-5f557.firebaseapp.com",
-  projectId: "webdashboard-5f557",
-  storageBucket: "webdashboard-5f557.appspot.com",
-  messagingSenderId: "1053170750729",
-  appId: "1:1053170750729:web:335cdfb159b8ea324b2fd8"
+     apiKey: "AIzaSyAczfyXu-N3s0MeatvLo1RfCXpR0L0FGg0",
+     authDomain: "liber-24ce7.firebaseapp.com",
+     databaseURL: "https://liber-24ce7-default-rtdb.firebaseio.com/",
+     projectId: "liber-24ce7",
+     storageBucket: "liber-24ce7.appspot.com",
+     messagingSenderId: "16217782629",
+     appId: "1:16217782629:web:6a4a155abc19c92e10c98c"
 };
 
 const app = initializeApp(firebaseConfig);
