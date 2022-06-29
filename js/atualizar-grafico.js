081b0027858f4da5ebdf3b0e6ea3d77da7a51bbd
lucas-pedrosa <lucas.dias.pedrosa@gmail.com>
2022-06-28 23:38:15 -0300

    Inserido Atualizar gráficos

---
+++ js/atualizar-grafico.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getDatabase, ref, get, set, child, push, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+import { firebaseConfig } from './firebase.js';
+
+const app= initializeApp(firebaseConfig);
+
+let atualizar = document.getElementById("btnAtualizar");
+let acao = document.getElementById("acao");
+let investigacao = document.getElementById("investigacao");
+let policial = document.getElementById("policial");
+let romance = document.getElementById("romance");
+let terror = document.getElementById("terror");
+
+atualizar.addEventListener('click', atualizarDados);
+
+function GenerosMaisRecomendados(acao, investigacao, policial, romance, terror) {
+  this.acao = acao;
+  this.investigacao = investigacao;
+  this.policial = policial;
+  this.romance = romance;
+  this.terror = terror;
+}
+
+const db = getDatabase();
+const starCountRef = ref(db, 'GenerosMaisRecomendados');
+onValue(starCountRef, (snapshot) => {
+  acao.value = snapshot.val().acao;
+  investigacao.value = snapshot.val().investigacao;
+  policial.value = snapshot.val().policial;
+  romance.value = snapshot.val().romance;
+  terror.value = snapshot.val().terror;
+});
+
+function atualizarDados() {
+
+  set(ref(db, 'GenerosMaisRecomendados'),{
+    acao:parseInt(acao.value),
+    investigacao: parseInt(investigacao.value),
+    policial: parseInt(policial.value),
+    romance: parseInt(romance.value),
+    terror: parseInt(terror.value)
+
+}).then(()=>{
+    console.log("atualizado com sucesso");
+})
+.catch((error)=>{
+    console.log("erro de inclusão");
+})
+}
--- js/firebase.js
+++ js/firebase.js
@@ -1,38 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, collection, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
-
-const firebaseConfig = {
-     apiKey: "AIzaSyAczfyXu-N3s0MeatvLo1RfCXpR0L0FGg0",
-     authDomain: "liber-24ce7.firebaseapp.com",
-     databaseURL: "https://liber-24ce7-default-rtdb.firebaseio.com/",
-     projectId: "liber-24ce7",
-     storageBucket: "liber-24ce7.appspot.com",
-     messagingSenderId: "16217782629",
-     appId: "1:16217782629:web:6a4a155abc19c92e10c98c"
+export const firebaseConfig = {
+  apiKey: "AIzaSyASv5h92BfWQ1LfYVeKLVjeCw0pbzCtizQ",
+  authDomain: "webdashboard-5f557.firebaseapp.com",
+  databaseURL: "https://webdashboard-5f557-default-rtdb.firebaseio.com",
+  projectId: "webdashboard-5f557",
+  storageBucket: "webdashboard-5f557.appspot.com",
+  messagingSenderId: "1053170750729",
+  appId: "1:1053170750729:web:335cdfb159b8ea324b2fd8"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-
-let enviar = document.getElementById("btnEnviar");
-let email = document.getElementById("email");
-let nome = document.getElementById("nome");
-let telefone = document.getElementById("telefone");
-let mensagem = document.getElementById("mensagem");
-
-enviar.addEventListener('click', inserirDados);
-
-async function inserirDados(){
-  var ref= doc(db,'Fale_Conosco',email.value);
-  const docRef = await setDoc(
-       ref,{
-       nome: nome.value,
-       telefone: telefone.value,
-       mensagem: mensagem.value
-  }).then(()=>{
-       console.log("incluído com sucesso");
-  })
-  .catch((error)=>{
-       console.log("erro de inclusão");
-  })
-}
